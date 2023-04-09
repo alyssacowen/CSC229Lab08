c3dee75f95798c350328bcd39b03f22b8a31a3d1
alyssacowen <alyssarcowen@gmail.com>
2023-04-08 20:45:19 -0400

    Created SinglyLinkedList Class Prompt user to enter an integer for n Set up the 2 singly linked lists (primes and primes with 3)

---
+++ Lab8/src/main/java/com/mycompany/lab8/Lab8.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab8;
+import java.util.*;
+/**
+ *
+ * @author alyss
+ */
+public class Lab8 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Please enter an integer for n: ");
+        int n = scnr.nextInt();
+        SinglyLinkedList primeNumbers = new SinglyLinkedList();
+        SinglyLinkedList primeNumbersThatContainAThree = new SinglyLinkedList();
+    }
+   
+    }
+}
