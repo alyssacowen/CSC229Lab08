7d0bde9d215415101813ff736ea0ffd72de0a2df
alyssacowen <alyssarcowen@gmail.com>
2023-04-08 21:22:47 -0400

    Created SinglyLinkedList Class Prompt user to enter an integer for n Set up the 2 singly linked lists (primes and primes with 3) Created isPrime and conatainsAThree functions Used for loop to append the prime numbers and prime numbers with a 3 into their proper lists

--- Lab8/src/main/java/com/mycompany/lab8/Lab8.java
+++ Lab8/src/main/java/com/mycompany/lab8/Lab8.java
@@ -15,9 +15,30 @@ public class Lab8 {
         int n = scnr.nextInt();
         SinglyLinkedList primeNumbers = new SinglyLinkedList();
         SinglyLinkedList primeNumbersThatContainAThree = new SinglyLinkedList();
+        
+         //starts at 2 because 2 is first prime number that comes after 0
+   for(int i = 2; i <= n; i++){
+       if(isPrime(i)){
+           primeNumbers.append(new Node(i));
+           if (containsAThree(i)){
+               primeNumbersThatContainAThree.append(new Node(i));
+           }
+       }
+   }
+   int sum = 0;
+   Node current = primeNumbersThatContainAThree.getHead();
+   while (current != null){
+       sum += current.data;
+       current = current.next;
+   }
+        System.out.println("The total sum of prime numbers that contain a 3 up until the integer " + n + " is equal to " + sum);
+    
+  
     }
+    
+    
         //check prime
-        public static boolean isPrime(int n){
+public static boolean isPrime(int n){
 if(n<=1)
 return false;
 else{
@@ -37,9 +58,10 @@ else{
             if (intToString.charAt(i)=='3'){
                 return true;
             }
-        
+        }
         return false;
-    }
-   
     
+    }
+}
+
 
