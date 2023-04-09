87d6acfe8970335e68a0e7cf6df6b44c030cbdac
alyssacowen <alyssarcowen@gmail.com>
2023-04-08 20:54:58 -0400

    Created SinglyLinkedList Class Prompt user to enter an integer for n Set up the 2 singly linked lists (primes and primes with 3) Created isPrime and conatainsAThree functions

--- Lab8/src/main/java/com/mycompany/lab8/Lab8.java
+++ Lab8/src/main/java/com/mycompany/lab8/Lab8.java
@@ -16,6 +16,30 @@ public class Lab8 {
         SinglyLinkedList primeNumbers = new SinglyLinkedList();
         SinglyLinkedList primeNumbersThatContainAThree = new SinglyLinkedList();
     }
-   
+        //check prime
+        public static boolean isPrime(int n){
+if(n<=1)
+return false;
+else{
+        for (int i = 2; i <= n / 2; i++) {
+
+            if (n % i == 0){ 
+                return false;
+            }
+        }
+        return true;
     }
-}
+        }
+        //check for 3
+    public static boolean containsAThree(int n){
+        String intToString = Integer.toString(n);
+        for (int i = 0; i < intToString.length(); i++){
+            if (intToString.charAt(i)=='3'){
+                return true;
+            }
+        
+        return false;
+    }
+   
+    
+
